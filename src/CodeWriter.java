0f1e395ee090e7b5013651e16dd131b110220679
aweditya <aweditya@gmail.com>
2021-12-22 16:48:52 +0530

    Completed some assembly translations

--- src/CodeWriter.java
+++ src/CodeWriter.java
@@ -1,16 +1,20 @@
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Generates assembly code from the parsed VM command
  */
 public class CodeWriter {
+    private final BufferedWriter writer;
+
     /**
      * Opens the output file/stream and gets ready to write into it
      *
-     * @param assemblyCode
+     * @param assemblyWriter
      */
-    public CodeWriter(File assemblyCode) {
-
+    public CodeWriter(FileWriter assemblyWriter) {
+        writer = new BufferedWriter(assemblyWriter);
     }
 
     /**
@@ -19,8 +23,77 @@ public class CodeWriter {
      *
      * @param command
      */
-    public void writeArithmetic(String command) {
+    public void writeArithmetic(String command) throws IOException {
+        String translation = "// " + command + "\n";
+        switch (command) {
+            case "add":
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "D=M\n";
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "M=D+M\n";
+                translation += "@SP\n";
+                translation += "M=M+1\n";
+                break;
+            case "sub":
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "D=M\n";
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "M=M-D\n";
+                translation += "@SP\n";
+                translation += "M=M+1\n";
+                break;
+            case "neg":
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "M=-M\n";
+                translation += "@SP\n";
+                translation += "M=M+1\n";
+                break;
+            case "eq":
 
+                break;
+            case "gt":
+
+                break;
+            case "lt":
+
+                break;
+            case "and":
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "D=M\n";
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "M=D&M\n";
+                translation += "@SP\n";
+                translation += "M=M+1\n";
+                break;
+            case "or":
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "D=M\n";
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "M=D|M\n";
+                translation += "@SP\n";
+                translation += "M=M+1\n";
+                break;
+            case "not":
+                translation += "@SP\n";
+                translation += "AM=M-1\n";
+                translation += "M=!M\n";
+                translation += "@SP\n";
+                translation += "M=M+1\n";
+                break;
+            default:
+                translation = "Not a valid arithmetic command\n";
+                break;
+        }
+        writer.write(translation);
     }
 
     /**
@@ -31,14 +104,139 @@ public class CodeWriter {
      * @param segment
      * @param index
      */
-    public void writePushPop(String command, String segment, int index) {
+    public void writePushPop(String command, String segment, int index) throws IOException {
+        String translation = "// " + command + " " + segment + " " + index + "\n";
+        if (command.equals("push")) {
+            switch (segment) {
+                case "local":
+                /*
+                Logic: address = LCL + index; *SP = *address; SP++;
+                 */
+                    translation += "@LCL\n"; // address=LCL+index
+                    translation += "D=M\n";
+                    translation += "@" + index + "\n";
+                    translation += "AD=D+A\n";
+                    translation += "D=M\n";
+                    translation += "@SP\n"; // *SP=*address
+                    translation += "@A=M\n";
+                    translation += "@M=D\n";
+                    translation += "@SP\n"; // SP++
+                    translation += "M=M+1\n";
+                    break;
+                case "argument":
+                /*
+                Logic: address = ARG + index; *SP = *address; SP++;
+                 */
+                    translation += "@ARG\n"; // address=ARG+index
+                    translation += "D=M\n";
+                    translation += "@" + index + "\n";
+                    translation += "AD=D+A\n";
+                    translation += "D=M\n";
+                    translation += "@SP\n"; // *SP=*address
+                    translation += "@A=M\n";
+                    translation += "@M=D\n";
+                    translation += "@SP\n"; // SP++
+                    translation += "M=M+1\n";
+                    break;
+                case "this":
+                /*
+                Logic: address = THIS + index; *SP = *address; SP++;
+                 */
+                    translation += "@THIS\n"; // address=THIS+index
+                    translation += "D=M\n";
+                    translation += "@" + index + "\n";
+                    translation += "AD=D+A\n";
+                    translation += "D=M\n";
+                    translation += "@SP\n"; // *SP=*address
+                    translation += "@A=M\n";
+                    translation += "@M=D\n";
+                    translation += "@SP\n"; // SP++
+                    translation += "M=M+1\n";
+                    break;
+                case "that":
+                /*
+                Logic: address = THAT + index; *SP = *address; SP++;
+                 */
+                    translation += "@THAT\n"; // address=THAT+index
+                    translation += "D=M\n";
+                    translation += "@" + index + "\n";
+                    translation += "AD=D+A\n";
+                    translation += "D=M\n";
+                    translation += "@SP\n"; // *SP=*address
+                    translation += "@A=M\n";
+                    translation += "@M=D\n";
+                    translation += "@SP\n"; // SP++
+                    translation += "M=M+1\n";
+                    break;
+                case "constant":
+                /*
+                Logic: *SP = index; SP++;
+                 */
+                    translation += "@" + index + "\n"; // D=index
+                    translation += "D=A";
+                    translation += "@SP"; // *SP=D
+                    translation += "A=M";
+                    translation += "M=D";
+                    translation += "@SP"; // SP++
+                    translation += "M=M+1";
+                    break;
+                case "static":
 
+                    break;
+                case "pointer":
+
+                    break;
+                case "temp":
+
+                    break;
+                default:
+                    translation = "Not a valid segment\n";
+                    break;
+            }
+        } else if (command.equals("pop")) {
+            if (segment.equals("local")) {
+                /*
+                Logic: address = LCL + index; SP--; *address = *SP;
+                 */
+                
+            } else if (segment.equals("argument")) {
+                /*
+                Logic: address = ARG + index; SP--; *address = *SP;
+                 */
+
+            } else if (segment.equals("this")) {
+                /*
+                Logic: address = THIS + index; SP--; *address = *SP;
+                 */
+
+            } else if (segment.equals("that")) {
+                /*
+                Logic: address = THAT + index; SP--; *address = *SP;
+                 */
+
+            } else if (segment.equals("static")) {
+
+            } else if (segment.equals("pointer")) {
+
+            } else if (segment.equals("temp")) {
+
+            } else {
+                translation = "Not a valid segment\n";
+            }
+        } else {
+            translation = "Not a push/pop command\n";
+        }
+        writer.write(translation);
     }
 
     /**
      * Closes the output file
      */
-    public void close() {
+    public void close() throws IOException {
+        writer.close();
+    }
+
+    public static void main(String[] args) {
 
     }
 }
