a92e6ba2cc3461d55aded9728e811d33c3407bc7
aweditya <aweditya@gmail.com>
2021-12-23 12:23:59 +0530

    Translator working for label, goto, if-goto instructions

--- src/VMTranslator.java
+++ src/VMTranslator.java
@@ -24,6 +24,13 @@ public class VMTranslator {
                 case "C_POP":
                     codeWriter.writePushPop("pop", parser.arg1(), parser.arg2());
                     break;
+                case "C_LABEL":
+                    codeWriter.writeLabel(parser.arg1());
+                    break;
+                case "C_GOTO":
+                    codeWriter.writeGoto(parser.arg1());
+                case "C_IF":
+                    codeWriter.writeIf(parser.arg1());
                 default:
                     break;
             }
@@ -32,7 +39,7 @@ public class VMTranslator {
     }
 
     public static void main(String[] args) throws IOException {
-        String vmPath = "../MemoryAccess/PointerTest/PointerTest.vm";
+        String vmPath = "../08/ProgramFlow/FibonacciSeries/FibonacciSeries.vm";
         File vmCode = new File(vmPath);
 
         String asmFileName = vmPath.substring(0, vmPath.lastIndexOf('.')) + ".asm";
